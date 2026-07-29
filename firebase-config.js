73960d0cc4315c113c7c9689100a5c30203e8ad0
salva <club.wave25@gmail.com>
2026-07-30 00:31:49 +0200

    Add Firebase config placeholder for requests feature

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.DJ_SALVA_FIREBASE_CONFIG = {
+  apiKey: "REPLACE_ME",
+  authDomain: "REPLACE_ME",
+  projectId: "REPLACE_ME",
+  storageBucket: "REPLACE_ME",
+  messagingSenderId: "REPLACE_ME",
+  appId: "REPLACE_ME"
+};
