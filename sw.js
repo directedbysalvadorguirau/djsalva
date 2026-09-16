8e5ff7bc8af703076b02024ecdee70f7004eb47c
salva <club.wave25@gmail.com>
2026-09-16 11:48:48 +0200

    Replace PWA artwork with DJ Salva icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "djsalva-pwa-v15";
+const CACHE_NAME = "djsalva-pwa-v16";
 const ASSETS = [
   "/",
   "/index.html",
@@ -16,6 +16,7 @@ const ASSETS = [
   "/icon3.png",
   "/icons/icon-192.png",
   "/icons/icon-512.png",
+  "/icons/apple-touch-icon.png",
   "/icons/maskable-512.png"
 ];
 
